2a163349fc26d7c37e61e992c54dd8cf5cf18fd8
liori <lior.itzhak@gmail.com>
2020-05-30 19:41:33 +0300

    init

--- reactive_service/ReactiveApp.py
+++ reactive_service/ReactiveApp.py
@@ -6,6 +6,16 @@ from kafka import KafkaConsumer
 import threading
 
 app = Sanic("hello_example")
+consumer = KafkaConsumer(
+    "activity",
+    bootstrap_servers="kafka-34f1d98c-sean98goldfarb-28b7.aivencloud.com:10402",
+    client_id="demo-client-1",
+    group_id=None,
+    security_protocol="SSL",
+    ssl_cafile="kafka_auth/ca.pem",
+    ssl_certfile="kafka_auth/service.cert",
+    ssl_keyfile="kafka_auth/service.key",
+)
 
 
 @app.route("/")
@@ -16,23 +26,16 @@ async def test(request):
 @app.route("/register/<user_id>/<type>")
 async def register(request, user_id, type):
     async def streaming_fn(response):
-        print(f'{user_id} {threading.current_thread().ident}')
-        topic = f'activity{user_id}{type}'
-        print(topic)
-        consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=['localhost:9092'],
-            enable_auto_commit=True,
-            group_id='my-group')
-        #
+        print(f'{user_id}-{type}-thread_id={threading.current_thread().ident}')  # DEBUG
         for message in consumer:
-            print(f'hi {user_id}')
-            message = message.value
-            print(message)
-            await response.write(message)
+            # TODO distributed filtering (Kafka-Stream-Api / Spark...)
+            if message['user_id'] == user_id and message['type'] == type:
+                message = message.value
+                print(message)
+                await response.write(message)
 
     return response.stream(streaming_fn, content_type='text/plain')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, workers=8)
+    app.run(host="0.0.0.0", port=1337, workers=4)
